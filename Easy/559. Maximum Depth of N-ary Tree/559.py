5d3d078432c88954134629721dc49fc356058944
vkayy <vkrice2475@gmail.com>
2023-11-26 23:38:21 +0000

    feat: update 559

--- Easy/559. Maximum Depth of N-ary Tree/559.py
+++ Easy/559. Maximum Depth of N-ary Tree/559.py
@@ -11,3 +11,8 @@ class Solution:
             return 0
         
         return 1 + max(self.maxDepth(child) for child in root.children) if root.children else 1
+
+# first, check if the root is None
+# if so, return 0
+
+# return 1 plus the maximum of the maximum depth of each child of the root if the root has children, otherwise 1
