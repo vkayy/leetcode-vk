013842e3bac28f537cdfe566b9313f4445d1e5f9
vkayy <vkrice2475@gmail.com>
2023-12-08 18:00:33 +0000

    feat: update 153

--- Medium/153. Find Minimum in Rotated Sorted Array/153.py
+++ Medium/153. Find Minimum in Rotated Sorted Array/153.py
@@ -2,48 +2,36 @@ from typing import List
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        if nums[0] <= nums[-1]:
-            return nums[0]
-        
-        mn = nums[0]
         l, r = 0, len(nums) - 1
-        
         while l <= r:
             if nums[l] <= nums[r]:
-                mn = min(mn, nums[l])
-                break
-            
+                return nums[l]
             m = (l + r) // 2
-            mn = min(mn, nums[m])
-            
-            if nums[m] >= nums[l]:
+            if nums[l] <= nums[m]:
                 l = m + 1
             else:
-                r = m - 1
-        
-        return mn
+                r = m
+        return nums[m]
 
-# first, check if the first element is less than or equal to the last element
-# if so, return the first element
+# set l and r to be 0 and len(nums) - 1
 
-# set the minimum to the first element
-# set the left pointer to zero
-# set the right pointer to the last index
+# while l is less than or equal to r
+# i.e., while there is a range to search
 
-# while the left pointer is less than the right pointer
-# if number at the left pointer is less than or equal to the number at the right pointer
-# set the minimum to the minimum of those two numbers
-# this is because we have found the minimum element
+# if the first element is less than or equal to the last element
+# i.e., if the range is sorted
+# return the first element as it is the minimum
 
-# otherwise, set the mid pointer to the average of the left and right pointers
-# set the minimum to the minimum of the number at the mid pointer and the minimum
+# set m to be the middle of l and r
 
-# if the number at the mid pointer is greater than or equal to the number at the left pointer
-# mid is in the left sorted array which is greater than the right sorted array
-# set the left pointer to the mid pointer plus one to search the right sorted array
+# if the first element is less than or equal to the middle element
+# i.e., if we are in the left part of the rotated array
+# set l to be m + 1
+# this is because the minimum is in the right half
 
-# otherwise, the number at the mid pointer is less than the number at the left pointer
-# mid is in the right sorted array which is less than the left sorted array
-# set the right pointer to the mid pointer minus one to search the left sorted array
+# otherwise, we are in the right part of the rotated array
+# set r to be m
+# this is because the minimum is in the left half
+# we don't want to set r to be m - 1 because m could be the minimum
 
-# return the minimum
+# return nums[m]
