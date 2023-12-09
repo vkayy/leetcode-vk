33c2cb4bf9b40b14776d9e1d9a1a1e4477b56dac
vkayy <vkrice2475@gmail.com>
2023-12-09 23:48:38 +0000

    feat: add 9

---
+++ Easy/9. Palindrome Number/9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        l, r = 0, len(x) - 1
+        while l < r:
+            if x[l] != x[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+# convert number to string
+# apply palindrome check
