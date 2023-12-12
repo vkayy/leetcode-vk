d4f3528297b23130109836b32b65a2011c8cf59b
vkayy <vkrice2475@gmail.com>
2023-12-12 17:44:56 +0000

    feat: add 2961

---
+++ Medium/2961. Double Modular Exponentiation/2961.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def getGoodIndices(self, variables: List[List[int]], target: int) -> List[int]:
+        res = []
+        for i, [a, b, c, m] in enumerate(variables):
+            if (a ** b % 10) ** c % m == target:
+                res.append(i)
+        return res
+
+# first, we can create a list to store the indices of the good variables
+
+# then, we can iterate through the variables
+# if the variable is good, we can append the index to the list
+
+# then, we can return the list
