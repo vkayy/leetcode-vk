0f51a06d4f605fdfede7453dc1c064bf54e77ad7
vkayy <vkrice2475@gmail.com>
2023-12-12 17:47:47 +0000

    feat: update 2961

--- Medium/2961. Double Modular Exponentiation/2961.py
+++ Medium/2961. Double Modular Exponentiation/2961.py
@@ -4,7 +4,7 @@ class Solution:
     def getGoodIndices(self, variables: List[List[int]], target: int) -> List[int]:
         res = []
         for i, [a, b, c, m] in enumerate(variables):
-            if (a ** b % 10) ** c % m == target:
+            if pow(pow(a, b, 10), c, m) == target:
                 res.append(i)
         return res
 
