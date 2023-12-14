bd35d26656a4d6dccb059891b4e790233d0749c4
vkayy <vkrice2475@gmail.com>
2023-12-14 16:20:24 +0000

    feat: update 530

--- Algorithms/Easy/530. Minimum Absolute Difference in BST/530.py
+++ Algorithms/Easy/530. Minimum Absolute Difference in BST/530.py
@@ -18,9 +18,10 @@ class Solution:
 
 # first, we define a helper function dfs that takes in a node, a minimum value, and a maximum value
 # if the node is None, we return the difference between the maximum and minimum values
+
 # we then recursively call dfs on the left and right children of the node
 # we return the minimum of the left and right subtrees
 # we call dfs on the root node, with the minimum value being negative infinity and the maximum value being positive infinity
 
 # the reason this works is because we are guaranteed that the left subtree will be less than the root node, and the right subtree will be greater than the root node
-# so, as the traversal reaches a leaf node, we can compare the difference between the maximum and minimum values seen in the traversal
+# so, as the traversal reaches a leaf node, we can find the difference between the current node and its parent node
