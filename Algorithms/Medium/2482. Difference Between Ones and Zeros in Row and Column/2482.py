e96b9733eae6dfd07bf11a0527a0f6381b9c64fe
vkayy <vkrice2475@gmail.com>
2023-12-14 13:39:31 +0000

    feat: update 2482

--- Algorithms/Medium/2482. Difference Between Ones and Zeros in Row and Column/2482.py
+++ Algorithms/Medium/2482. Difference Between Ones and Zeros in Row and Column/2482.py
@@ -3,15 +3,16 @@ from itertools import product
 
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        m, n = len(grid), len(grid[0])
         rows, cols = [], []
         for row in grid:
-            rows.append((row.count(1), row.count(0)))
+            rows.append(row.count(1))
         for col in zip(*grid):
-            cols.append((col.count(1), col.count(0)))
-        for i, j in product(range(len(grid)), range(len(grid[0]))):
-            grid[i][j] = rows[i][0] + cols[j][0] - rows[i][1] - cols[j][1]
+            cols.append(col.count(1))
+        for i, j in product(range(m), range(n)):
+            grid[i][j] = rows[i] * 2 + cols[j] * 2 - m - n
         return grid
 
-# first, we count the number of 1s and 0s in each row and column
-# then, we use the formula to calculate the difference in each cell
-# we return the grid after the calculation
+# first, we count the number of 1s in each row and column
+# then, we use the formula
+# we return the grid
